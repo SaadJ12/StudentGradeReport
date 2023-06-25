12243fcdde4e529c6ce85ff406fb8f7e7c693ea0
Saad <saadjamshaid000@gmail.com>
2023-06-26 00:49:09 +0500

    Implement while loop for continuous student enrollment

--- ITCProject/StudentGradeReport/StudentGradeReport0172.cpp
+++ ITCProject/StudentGradeReport/StudentGradeReport0172.cpp
@@ -1,27 +1,47 @@
 #include <iostream>
 using namespace std;
 int main() {
-	int computer, maths, roll;
+	int computer, maths, roll, studentNumber = 1;
 	char decision;
 	bool flag = true;
 	cout << "Welcome to Admin Panel" << endl;
 	cout << "Enter Roll Numbers and their data to enroll students" << endl;
 
 	while(flag) {
-		cout << "Enter Roll Number of student 1: ";
+		int student[4];
+		cout << "Enter Roll Number of student " << studentNumber << ": ";
 		cin >> roll;
 		cout << "Enter Marks of Computer Science of student 1 within 100: ";
 		cin >> computer;
+		while (computer > 100) {
+		cout << "Enter Marks of Computer Science of student 1 within 100: ";
+		cin >> computer;
+		}
+		
 		cout << "Enter Marks of Maths of student 1 within 100: ";
 		cin >> maths;
+		while (maths > 100) {
+			cout << "Enter Marks of Maths of student 1 within 100: ";
+			cin >> maths;
+		}
+		
 		cout << "Enter Y / y if you continue adding students and their data and N/n to stop: ";
 		cin >> decision;
 		if (decision == 'Y' || decision == 'y') {
 			flag = true;
+			studentNumber++;
 		}
 		else {
+			studentNumber = student[0];
+			roll = student[1];
+			computer = student[2];
+			maths = student[3];
 			flag = false;
 		}
+		cout << "Roll Number of student" << student[0] << "is" << student[1];
+		cout << "Marks of Computer Science of " << student[0] << "is" << student[2];
+		cout << "Marks of Maths of " << student[0] << "is" << student[3];
 	}
+	system("pause");
 	return 0;
 }
