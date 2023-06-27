8ec34d00622642fcc2b0809f3469617d8b903f44
Saad <saadjamshaid000@gmail.com>
2023-06-28 02:13:41 +0500

    Refactor program to use separate arrays for student data and add functionality to change student's roll number

--- ITCProject/StudentGradeReport/StudentGradeReport0172.cpp
+++ ITCProject/StudentGradeReport/StudentGradeReport0172.cpp
@@ -1,47 +1,87 @@
 #include <iostream>
 using namespace std;
-int main() {
-	int computer, maths, roll, studentNumber = 1;
-	char decision;
-	bool flag = true;
-	cout << "Welcome to Admin Panel" << endl;
-	cout << "Enter Roll Numbers and their data to enroll students" << endl;
 
-	while(flag) {
-		int student[4];
-		cout << "Enter Roll Number of student " << studentNumber << ": ";
-		cin >> roll;
-		cout << "Enter Marks of Computer Science of student 1 within 100: ";
-		cin >> computer;
-		while (computer > 100) {
-		cout << "Enter Marks of Computer Science of student 1 within 100: ";
-		cin >> computer;
-		}
-		
-		cout << "Enter Marks of Maths of student 1 within 100: ";
-		cin >> maths;
-		while (maths > 100) {
-			cout << "Enter Marks of Maths of student 1 within 100: ";
-			cin >> maths;
-		}
-		
-		cout << "Enter Y / y if you continue adding students and their data and N/n to stop: ";
-		cin >> decision;
-		if (decision == 'Y' || decision == 'y') {
-			flag = true;
-			studentNumber++;
-		}
-		else {
-			studentNumber = student[0];
-			roll = student[1];
-			computer = student[2];
-			maths = student[3];
-			flag = false;
-		}
-		cout << "Roll Number of student" << student[0] << "is" << student[1];
-		cout << "Marks of Computer Science of " << student[0] << "is" << student[2];
-		cout << "Marks of Maths of " << student[0] << "is" << student[3];
-	}
-	system("pause");
-	return 0;
+int main() {
+    int computer[1000], maths[1000], roll[1000], studentNumber = 0, options, checkRoll, updateRoll;
+    float marks[1000], percentage[1000];
+    char decision, grading;
+    bool flag = true;
+
+    cout << "Welcome to Admin Panel" << endl;
+    cout << "Enter Roll Numbers and their data to enroll students" << endl;
+
+    while (flag) {
+      studentNumber++;
+
+      cout << "Enter Roll Number of student " << studentNumber << ": ";
+      cin >> roll[studentNumber - 1];
+      cout << "Enter Marks of Computer Science of student " << studentNumber << " within 100: ";
+      cin >> computer[studentNumber - 1];
+      while (computer[studentNumber - 1] > 100) {
+      cout << "Enter Marks of Computer Science of student " << studentNumber << " within 100: ";
+      cin >> computer[studentNumber - 1];
+      }
+
+      cout << "Enter Marks of Maths of student " << studentNumber << " within 100: ";
+      cin >> maths[studentNumber - 1];
+      while (maths[studentNumber - 1] > 100) {
+      cout << "Enter Marks of Maths of student " << studentNumber << " within 100: ";
+      cin >> maths[studentNumber - 1];
+      }
+
+      marks[studentNumber - 1] = computer[studentNumber - 1] + maths[studentNumber - 1];
+      percentage[studentNumber - 1] = marks[studentNumber - 1] / 2;
+
+      if (percentage[studentNumber - 1] >= 91 && percentage[studentNumber - 1] <= 100) {
+            grading = 'A';
+       }
+      else if (percentage[studentNumber - 1] >= 75 && percentage[studentNumber - 1] <= 90) {
+            grading = 'B';
+        }
+      else if (percentage[studentNumber - 1] >= 60 && percentage[studentNumber - 1] < 75) {
+            grading = 'C';
+      }
+      else if (percentage[studentNumber - 1] >= 50 && percentage[studentNumber - 1] < 60) {
+            grading = 'D';
+        }
+      else if (percentage[studentNumber - 1] >= 0 && percentage[studentNumber - 1] < 50) {
+            grading = 'F';
+       }
+
+        cout << "Enter Y/y if you continue adding students and their data and N/n to stop: ";
+        cin >> decision;
+
+        if (decision == 'Y' || decision == 'y') {
+            flag = true;
+        }
+        else {
+            flag = false;
+        }
+    }
+
+    cout << endl << "Student Data:" << endl;
+    for (int i = 0; i < studentNumber; i++) {
+        cout << "Roll Number of student " << (i + 1) << " is: " << roll[i] << endl;
+        cout << "Marks of Computer Science of student " << (i + 1) << " is: " << computer[i] << endl;
+        cout << "Marks of Maths of student " << (i + 1) << " is: " << maths[i] << endl;
+        cout << "Percentage of student " << (i + 1) << " is: " << percentage[i] << endl;
+        cout << "Grade of student " << (i + 1) << " is: " << grading << endl;
+        cout << endl;
+    }
+    cout << "Press 1 to update Roll Number of a particular Student.";
+    cin >> options;
+    if (options == 1) {
+        cout << "Enter the roll of a Student: ";
+        cin >> checkRoll;
+        for (int i = 0; i < studentNumber; i++) {
+            if (checkRoll == roll[i]){
+                cout << "Enter the new Roll Number of a Student: ";
+                cin >> updateRoll;
+                roll[i] = updateRoll;
+                cout << "Roll Number has been updated to: " << roll[i] << endl;
+            }
+        }
+    }
+    system("pause");
+    return 0;
 }
